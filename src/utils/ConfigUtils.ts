3b571a84c867a0816013562e2852f6b1f50b964d
Elliot Yoon <elliotyoon@outlook.com>
2023-05-28 18:17:38 -0700

    Added abstract class, enabled global command registration, updated interface, moved reminder abstract class to parent

--- src/utils/ConfigUtils.ts
+++ src/utils/ConfigUtils.ts
@@ -1,9 +1,11 @@
 import { existsSync, readFileSync } from "node:fs";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
+import { Logger } from "winston";
 import YAML from "yaml";
 
 /**
+ * You probably want BotWithConfig instead of this, which has config methods you can call.
  * Does file URL/path stuff to get the path string of a dir/file.
  * @param importUrl `import.meta`
  * @param fileName If string, the file name to get the path of. If null, this will just get a dir.
@@ -19,17 +21,19 @@ export function getPath(importMeta: ImportMeta, fileName: string | null): string
 }
 
 /**
+ * You probably want BotWithConfig instead of this, which has config methods you can call.
  * Attempts to read the YAML config file for this bot with the specified file name. Throws an error if it is unable to do so.
  * @param importMeta `import.meta`
  * @param path The YAML config file path.
+ * @param logger The winston Logger to use.
  * @returns The config object with the type passed in.
  */
-export async function readYamlConfig<ConfigType>(importMeta: ImportMeta, fileName: string): Promise<ConfigType> {
+export function readYamlConfig<ConfigType>(importMeta: ImportMeta, fileName: string, logger: Logger): ConfigType {
     const path = getPath(importMeta, fileName);
 
     if (!existsSync(path)) {
         const errMsg = `Configuration was not found: ${path}`;
-        console.error(errMsg);
+        logger.error(errMsg);
         throw new Error(errMsg);
     }
 
@@ -40,7 +44,7 @@ export async function readYamlConfig<ConfigType>(importMeta: ImportMeta, fileNam
         config = YAML.parse(configFileText);
     } catch (error) {
         const errMsg = `Error during configuration loading: ${path}`;
-        console.error(errMsg);
+        logger.error(errMsg);
         throw new Error(errMsg);
     }
 
